401cadc0c7d29e5cf269509d2b736f76aec9cced
BoyanDimov20 <44165461+BoyanDimov20@users.noreply.github.com>
2021-04-01 12:49:29 +0300

    Upload Image from Modal

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+var modal = document.getElementById("myModal");
+
 document.getElementById('uploadButton').addEventListener('click', function(event) {
     var ctx = document.getElementById('drawing_canvas');
 
-    var image = new Image(950, 950);
+    var currentWidth = Number(ctx.width);
+    var currentHeight = Number(ctx.height);
+
+    var image = new Image(currentWidth, currentHeight);
     var file = document.getElementById('uploadFile');
     image.src = file.value;
     image.onload = function() {
-        ctx.getContext('2d').drawImage(image, 0, 0, 950, 950);
+        ctx.getContext('2d').drawImage(image, 0, 0, currentWidth, currentHeight);
     }
-})
+    modal.style.display = 'none';
+});
+
+
+document.getElementById('open_file_button').addEventListener('click', function(e) {
+    modal.style.display = 'block';
+});
+
+document.getElementsByClassName('close')[0].addEventListener('click', function(e) {
+    modal.style.display = 'none';
+});
