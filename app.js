e14e663e85ae647b18b435f3882d12c6719c0baf
BoyanDimov20 <44165461+BoyanDimov20@users.noreply.github.com>
2021-04-05 18:14:51 +0300

    Refactoring

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-document.getElementById('uploadButton').addEventListener('click', function(event) {
-    var ctx = document.getElementById('drawing_canvas');
+const modal = document.getElementById("myModal");
+const ctx = document.getElementById('drawing_canvas');
 
-    var image = new Image(950, 950);
-    var file = document.getElementById('uploadFile');
+document.getElementById('uploadButton').addEventListener('click', function(event) {
+
+    const currentWidth = Number(ctx.width);
+    const currentHeight = Number(ctx.height);
+
+    const image = new Image(currentWidth, currentHeight);
+    const file = document.getElementById('uploadFile');
     image.src = file.value;
     image.onload = function() {
         ctx.getContext('2d').drawImage(image, 0, 0, 950, 950);
     }
-})
+    modal.style.display = 'none';
+    file.value = '';
+});
+
+document.getElementById('open_file_button').addEventListener('click', function(e) {
+    modal.style.display = 'block';
+});
+
+document.getElementsByClassName('close')[0].addEventListener('click', function(e) {
+    modal.style.display = 'none';
+});
+
+document.getElementById('save_file_button').addEventListener('click', function(e) {
+    const link = document.createElement('a');
+    link.download = 'image.png';
+    link.href = ctx.toDataURL();
+    link.click();
+});
