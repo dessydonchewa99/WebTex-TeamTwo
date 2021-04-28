a9f425181de56226e32eb1f30830d50ff7af2f6c
Pkotova <petya.kotova@abv.bg>
2021-04-28 18:17:51 +0300

    login reg

--- test.js
+++ test.js
@@ -1,98 +1,84 @@
-const canvas = document.querySelector('#drawing_canvas');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("drawing_canvas");
+const ctx = canvas.getContext("2d");
+let coord = { x: 0, y: 0 };
 
-var color,
-    drawing = false,
-    drawMode,
-    tempX,
-    tempY,
-    position;
-
-document.getElementById('rectangle_button').addEventListener('click',function (e){
-        console.log("hello")
-        canvas.addEventListener('mousedown', drawingMode, true);
-        canvas.addEventListener('mousemove', drawingRectangleMode, true);
-        canvas.addEventListener('mouseup', drawingBreak, true);
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    });
-
-function drawingMode(event) {
-    drawing = true;
-    drawMode = positions(event);
-    color = 'blue';
-}
-function drawingRectangleMode(event) {
-    if (drawing === true) {
-        position = positions(event);
-        drawRectangle(position);
-        context.fill();
+//free hand draw button
+document.getElementById('freehand_draw_button').addEventListener("click",function (r){
+    document.addEventListener("mousedown", start)
+    {
+        console.log("FREE HAND BUTTON")
     }
-}
-function drawingBreak(event) {
-    drawing = false;
-    position = positions(event);
-    drawRectangle(position);
-    context.fill();
-    tempRectangle = {
-        x:tempX,
-        y:tempY,
-    };
-}
-function drawRectangle(position) {
-    tempX = drawMode.x;
-    tempY = drawMode.y;
-
-    context.beginPath();
-    context.rect(tempX, tempY, position.x, position.y);
-    context.closePath();
-}
-function positions(event) {
-    var x = event.clientX - canvas.getBoundingClientRect().left,
-        y = event.clientY - canvas.getBoundingClientRect().top;
-    return {
-        x: x,
-        y: y
-    };
-}
-
-//circle
-document.getElementById('circle_button').addEventListener('click',function (e){
-    canvas.addEventListener('mousedown', drawingMode1, false);
-    canvas.addEventListener('mousemove', drawingCircleMode, false);
-    canvas.addEventListener('mouseup', drawingBreak1, false);
-    canvas.width = window.innerWidth * 0.5;
-    canvas.height = window.innerHeight * 0.5;
-});
-function drawingMode1(event) {
-    drawing = true;
-    drawMode = positions(event);
-    color = 'black';
-}
-function drawingCircleMode(event) {
-    if (drawing === true) {
-        position = positions(event);
-        drawCircle(position);
-        context.fill();
+    document.addEventListener("mouseup", stop);
+})
+// line button
+document.getElementById('line_button').addEventListener("click",function (r){
+    document.addEventListener("mousedown", start)
+    {
+        console.log("LINE BUTTON")
     }
-}
-function drawingBreak1(event) {
-    drawing = false;
-    position = positions(event);
-    drawCircle(position);
-    context.fill();
-    tempCircle = {
-        x:tempX,
-        y:tempY,
-        rad:radius
-    };
-}
-function drawCircle(position) {
-    tempX = drawMode.x;
-    tempY = drawMode.y;
+    document.addEventListener("mouseup", stop);
+})
+// non-filled rectangle
+document.getElementById('rectangle_button').addEventListener("click",function (r){
+    document.addEventListener("mousedown", start)
+    {
+        console.log("RECTANGLE BUTTON")
+    }
+    document.addEventListener("mouseup", stop);
+})
 
-    radius = Math.sqrt(Math.pow((tempX - position.x), 2) + Math.pow((tempY - position.y), 2));
-    context.beginPath();
-    context.arc(tempX, tempY, radius, 0, 2 * Math.PI, false);
-    context.closePath();
+//filled rectangle
+document.getElementById('filled_rectangle_button').addEventListener("click",function (r){
+    document.addEventListener("mousedown", start)
+    {
+        console.log("FILLED RECTANGLE BUTTON")
+    }
+    document.addEventListener("mouseup", stop);
+})
+
+//non-filled circle
+document.getElementById('circle_button').addEventListener("click",function (r){
+    document.addEventListener("mousedown", start)
+    {
+        console.log(" CIRCLE BUTTON")
+    }
+    document.addEventListener("mouseup", stop);
+})
+//filled circle
+document.getElementById('filled_circle_button').addEventListener("click",function (r){
+    document.addEventListener("mousedown", start)
+    {
+        console.log(" FILLED CIRCLE BUTTON")
+    }
+    document.addEventListener("mouseup", stop);
+})
+
+window.addEventListener("resize", resize);
+
+resize();
+
+function resize() {
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+}
+function reposition(event) {
+    coord.x = event.clientX - canvas.offsetLeft;
+    coord.y = event.clientY - canvas.offsetTop;
+}
+function start(event) {
+    document.addEventListener("mousemove", drawFreeHandLine);
+    reposition(event);
+}
+function stop() {
+    document.removeEventListener("mousemove", drawFreeHandLine);
+}
+function drawFreeHandLine(event) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "black";
+    ctx.moveTo(coord.x, coord.y);
+    reposition(event);
+    ctx.lineTo(coord.x, coord.y);
+    ctx.stroke();
 }
